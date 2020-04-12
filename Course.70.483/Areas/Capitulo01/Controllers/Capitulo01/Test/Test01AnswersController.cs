eac4227cf533ec41b6c8b9290dc2cb2c98c5f08d
Saúl Rincón <saulrincon@hotmail.com>
2020-04-11 22:05:45 -0500

    Working on test and answers

---
+++ Course.70.483/Areas/Capitulo01/Controllers/Capitulo01/Test/Test01AnswersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course._70._483.Areas.Capitulo01.Controllers.Capitulo01.Test
+{
+    public class Test01AnswersController : Controller
+    {
+        [Area("Capitulo01")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
